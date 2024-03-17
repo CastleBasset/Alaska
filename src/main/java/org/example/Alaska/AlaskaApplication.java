d0944f1b1bb53d97dcd6f996c921e87c9449e8e3
futuxela <futuxela@gmail.com>
2024-03-17 12:24:37 +0300

    init project

---
+++ src/main/java/org/example/Alaska/AlaskaApplication.java
@@ -0,0 +1,13 @@
+package org.example.Alaska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlaskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlaskaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/Alaska/AlaskaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.Alaska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlaskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
