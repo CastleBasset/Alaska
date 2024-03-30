64460fd7d7a395238f2fee968106d700fbb876ad
CastleBasset <futuxela@gmail.com>
2024-03-30 16:22:12 +0300

    MainWindow + panels

--- src/main/java/org/example/alaska/AlaskaApplication.java
+++ src/main/java/org/example/alaska/AlaskaApplication.java
@@ -3,11 +3,27 @@ package org.example.alaska;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import javax.swing.*;
+import java.awt.*;
+
 public class AlaskaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlaskaApplication.class, args);
+	public static void main(String[] args)
+	{
+		JFrame mainWindow = new JFrame("soobakaaaaa");
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setSize(900,900);
+
+		JPanel pesPanel = new JPanel();
+		pesPanel.setBackground(Color.ORANGE);
+		mainWindow.add(pesPanel);
+		JPanel sobakaPanel = new JPanel();
+		pesPanel.setBackground(Color.CYAN);
+		pesPanel.setPreferredSize(new Dimension(100,100));
+		mainWindow.add(sobakaPanel);
+
+		mainWindow.setVisible(true);
+		//SpringApplication.run(AlaskaApplication.class, args);
 	}
 
 }
